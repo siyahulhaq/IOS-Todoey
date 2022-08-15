24e23e9df08b1db8c85e56f9657e82832f888bc4
Siyahul Haq <76936407+siyahulhaq-ceymox@users.noreply.github.com>
2022-08-15 11:49:33 +0530

    added core data

--- Todoey/Controllers/TodoViewController.swift
+++ Todoey/Controllers/TodoViewController.swift
@@ -9,44 +9,69 @@ import UIKit
 
 class TodoViewController: UITableViewController {
     
-    let defaults = UserDefaults.standard
+    var items = [TodoItem]()
     
-    var items = [String]()
-
+    let datatFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("TodoItems.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let safeItems = defaults.object(forKey: "Todos") as? [String] {
-            items = safeItems
-        }
+        loadItems()
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     
-    //Mark: Table View delegate methods
+    //MARK: - Load Items from Plist
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: datatFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                items = try decoder.decode([TodoItem].self, from: data)
+            } catch {
+                print("Error Decding todo items from plist \(error)")
+            }
+        }
+    }
+    
+    //MARK: - Save or Update item
+    
+    func saveItem() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.items)
+            if let safeDataFilePath = self.datatFilePath {
+                try data.write(to: safeDataFilePath)
+            }
+        } catch {
+            print("DATABASE writting Error \(error)")
+        }
+    }
+    
+    //MARK: - Table View delegate methods
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCellView",for: indexPath)
-        cell.textLabel?.text = self.items[indexPath.row]
+        cell.textLabel?.text = self.items[indexPath.row].title
+        let item = items[indexPath.row]
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
-    //Mark: Onselect row
+    //MARK: - Onselect row
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = self.items[indexPath.row]
+        item.done = !item.done
+        self.saveItem()
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
-        let cellForRow = tableView.cellForRow(at: indexPath)
-        if cellForRow?.accessoryType == .checkmark {
-            cellForRow?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
     }
     
-    //Mark: Add new Item
+    //MARK: - Add new Item
     
     @IBAction func addTodoItem(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add Todo", message: "Here you can add todo", preferredStyle: .alert)
@@ -56,8 +81,8 @@ class TodoViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default){ (action) in
             print("Added")
             if let safeTextField = textField {
-                self.items.append(safeTextField.text!)
-                self.defaults.set(self.items, forKey: "Todos")
+                self.items.append(TodoItem(title: safeTextField.text!, done: false))
+                self.saveItem()
                 self.tableView.reloadData()
             }
         }
@@ -72,6 +97,6 @@ class TodoViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-
+    
 }
 
